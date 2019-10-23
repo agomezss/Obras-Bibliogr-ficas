5ec0e67879e21ff9237606a8fd6b2d8a4c9a57c5
agomezss <a.gomezsimoes@outlook.com>
2019-10-23 10:40:34 -0300

    teste unitario das regras completas

--- Obras.Bibliograficas.Alessandro/Obras.Bibliograficas.Alessandro.Test/AutorNomeProviderPadraoTest.cs
+++ Obras.Bibliograficas.Alessandro/Obras.Bibliograficas.Alessandro.Test/AutorNomeProviderPadraoTest.cs
@@ -1,3 +1,4 @@
+using Obras.Bibliograficas.Alessandro.Domain.Autores.Nome;
 using System;
 using Xunit;
 
@@ -6,9 +7,73 @@ namespace Obras.Bibliograficas.Alessandro.Test
 	public class AutorNomeProviderPadraoTest
 	{
 		[Fact]
-		public void Test1()
+		public void TestStringVaziaNaoCausaException()
 		{
+			// Arrange
+			var entrada = "";
+			var regra = new AutorNomeProviderPadrao();
 
+			// Act
+			var resultado = regra.AplicarRegrasNome(entrada);
+
+			// Assert
+			Assert.Equal("", resultado);
+		}
+
+		[Fact]
+		public void TestNomeUnico()
+		{
+			// Arrange
+			var entrada = "gomez";
+			var regra = new AutorNomeProviderPadrao();
+
+			// Act
+			var resultado = regra.AplicarRegrasNome(entrada);
+
+			// Assert
+			Assert.Equal("GOMEZ", resultado);
+		}
+
+		[Fact]
+		public void TestNomeComSobrenomeUnico()
+		{
+			// Arrange
+			var entrada = "alessandro gomez";
+			var regra = new AutorNomeProviderPadrao();
+
+			// Act
+			var resultado = regra.AplicarRegrasNome(entrada);
+
+			// Assert
+			Assert.Equal("GOMEZ, Alessandro", resultado);
+		}
+
+		[Fact]
+		public void TestNomeComMultiplosSobrenomes()
+		{
+			// Arrange
+			var entrada = "alessandro gomez santiago";
+			var regra = new AutorNomeProviderPadrao();
+
+			// Act
+			var resultado = regra.AplicarRegrasNome(entrada);
+
+			// Assert
+			Assert.Equal("SANTIAGO, Alessandro Gomez", resultado);
+		}
+
+		[Fact]
+		public void TestNomeComTerminacaoFamiliar()
+		{
+			// Arrange
+			var entrada = "alessandro gomez filho";
+			var regra = new AutorNomeProviderPadrao();
+
+			// Act
+			var resultado = regra.AplicarRegrasNome(entrada);
+
+			// Assert
+			Assert.Equal("GOMEZ FILHO, Alessandro", resultado);
 		}
 	}
 }
